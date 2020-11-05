f9e59ca9ff9828ce853faa599a9c09f7931a478f
eggyatma2908 <eggyatmariansyah@gmail.com>
2020-11-05 12:28:53 +0700

    Completed

--- Week 3/3.js
+++ Week 3/3.js
@@ -1,13 +1,10 @@
 const divideAndSort = (input) => {
     if (typeof input === 'number') {
-        let p1 = input.toString().split("0") // ["595656", "159466", "56" ]
-        let p2 = p1[0].split("").sort() // [ "5", "5", "5", "6", "6", "9" ]
-        let p3 = p1[1].split("").sort() // [ "1", "4", "5", "6", "6", "9" ]
-        let p4 = p1[2].split("").sort() // [ "5", "6" ]
-        let p5 = [...p2, ...p3, ...p4]  // [ "5", "5", "5", "6", "6", "9", "1", "4", "5", "6", "6", "9", "5", "6" ]
-        console.log(p5.join(""))
+        let p1 = input.toString().split("0"); // ["595656", "159466", "56" ]
+        let sortir = p1.map((angka) => angka.split("").sort().join("")); // [ "555669", "145669", "56" ]
+        console.log(sortir.join(""));
     } else {
-        console.log("Ini Bukan Angka")
+        console.log("Ini Bukan Angka");
     }
 }
 
